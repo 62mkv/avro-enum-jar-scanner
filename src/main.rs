356e711d3cabfd10fe3ad58c1b4ad44e214ef14e
62mkv <62mkv@mail.ru>
2022-10-29 17:47:29 +0300

    WIP: added classpath.idx extension and class scanning from "classes" directory first (1h)
    had to fight borrow checker a bit - see Vec::with_capacity; without that I was getting E0502 (mutable borrow while immutable borrow is still active)

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use noak::AccessFlags;
 use noak::reader::Class;
 use regex::Regex;
 use serde_json::to_string;
+use zip::read::ZipFile;
 
 use evaluator::RegexEvaluator;
 use visitor::{ClassSource, EnumVisitor};
@@ -23,37 +24,63 @@ struct Cli {
     jarfile: PathBuf,
 
     #[arg(short, long, value_name = "REGEX")]
-    class_name_regex: Option<Regex>
+    class_name_regex: Option<Regex>,
 }
 
 fn list_zip_contents(reader: impl Read + Seek, source: &ClassSource, class_name_evaluator: &RegexEvaluator, enum_visitor: &mut EnumVisitor) -> anyhow::Result<()> {
     let mut zip = zip::ZipArchive::new(reader)?;
 
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        if file.is_file() {
-            let class_file_name = &(file.name().to_owned());
-            let class_file_name = if matches!(source, ClassSource::Root) {
-                class_file_name.trim_start_matches("BOOT-INF/classes/")
-            } else {
-                class_file_name
-            };
-            if class_file_name.ends_with(".class") && class_name_evaluator.evaluate_if_class_needed(class_file_name)? {
-                let mut data = Vec::new();
-                file.read_to_end(&mut data)?;
-                let mut class = Class::new(&*data)?;
-                let is_enum = class.access_flags()?.contains(AccessFlags::ENUM);
-                if is_enum {
-                    enum_visitor.visit_enum(&mut class, source)?;
-                }
-            } else if class_file_name.ends_with(".jar") {
-                let mut data = Vec::new();
-                file.read_to_end(&mut data)?;
-                list_zip_contents(io::Cursor::new(data), &ClassSource::NestedJar(String::from(file.name())), class_name_evaluator, enum_visitor)?;
+    let classpath_index_found = zip.by_name("BOOT-INF/classpath.idx").ok().is_some();
+    if classpath_index_found {
+        println!("Classpath index file is found!");
+    }
+
+    if matches!(source, ClassSource::Root) && classpath_index_found {
+        let mut file_names: Vec<String> = Vec::with_capacity(100);
+
+        let zip_iterator = zip.file_names();
+        for file_name in zip_iterator {
+            if file_name.starts_with("BOOT-INF/classes/") {
+                file_names.push(String::from(file_name));
             }
+        }
+
+        for file_name in file_names {
+            let mut file = zip.by_name(&file_name)?;
+            process_file_from_zip(source, class_name_evaluator, enum_visitor, &mut file)?;
+        }
+    } else {
+        for i in 0..zip.len() {
+            let mut file = zip.by_index(i)?;
+            process_file_from_zip(source, class_name_evaluator, enum_visitor, &mut file)?;
         }
     }
 
+    Ok(())
+}
+
+fn process_file_from_zip(source: &ClassSource, class_name_evaluator: &RegexEvaluator, enum_visitor: &mut EnumVisitor, file: &mut ZipFile) -> anyhow::Result<()> {
+    if file.is_file() {
+        let class_file_name = &(file.name().to_owned());
+        let class_file_name = if matches!(source, ClassSource::Root) {
+            class_file_name.trim_start_matches("BOOT-INF/classes/")
+        } else {
+            class_file_name
+        };
+        if class_file_name.ends_with(".class") && class_name_evaluator.evaluate_if_class_needed(class_file_name)? {
+            let mut data = Vec::new();
+            file.read_to_end(&mut data)?;
+            let mut class = Class::new(&*data)?;
+            let is_enum = class.access_flags()?.contains(AccessFlags::ENUM);
+            if is_enum {
+                enum_visitor.visit_enum(&mut class, source)?;
+            }
+        } else if class_file_name.ends_with(".jar") {
+            let mut data = Vec::new();
+            file.read_to_end(&mut data)?;
+            list_zip_contents(io::Cursor::new(data), &ClassSource::NestedJar(String::from(file.name())), class_name_evaluator, enum_visitor)?;
+        }
+    }
     Ok(())
 }
 
